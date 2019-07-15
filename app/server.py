9e671acd55c6d22546d4252b286160697f6d6104
asiedubrempong <kaybrempong@gmail.com>
2019-07-15 21:58:54 +0000

    initial commit

---
+++ app/server.py
@@ -0,0 +1,90 @@
+import aiohttp
+import asyncio
+import uvicorn
+from fastai import *
+from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
+
+export_file_url = 'https://drive.google.com/uc?export=download&id=1xcTpai7PgVgwrm17x6E0YT3Rshjhm_vY'
+export_file_name = 'final.pkl'
+
+classes = ['Black Pod', 'Healthy Pod']
+path = Path(__file__).parent
+
+app = Starlette()
+app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.mount('/static', StaticFiles(directory='app/static'))
+
+
+async def download_file(url, dest):
+    if dest.exists(): return
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            data = await response.read()
+            with open(dest, 'wb') as f:
+                f.write(data)
+
+
+async def setup_learner():
+    await download_file(export_file_url, path / export_file_name)
+    try:
+        learn = load_learner(path, export_file_name)
+        return learn
+    except RuntimeError as e:
+        if len(e.args) > 0 and 'CPU-only machine' in e.args[0]:
+            print(e)
+            message = "\n\nThis model was trained with an old version of fastai and will not work in a CPU environment.\n\nPlease update the fastai library in your training environment and export your model again.\n\nSee instructions for 'Returning to work' at https://course.fast.ai."
+            raise RuntimeError(message)
+        else:
+            raise
+
+
+loop = asyncio.get_event_loop()
+tasks = [asyncio.ensure_future(setup_learner())]
+learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
+loop.close()
+
+
+@app.route('/')
+async def homepage(request):
+    html_file = path / 'view' / 'index.html'
+    return HTMLResponse(html_file.open().read())
+
+
+@app.route('/analyze', methods=['POST'])
+async def analyze(request):
+    img_data = await request.form()
+    img_bytes = await (img_data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction = learn.predict(img)[0]
+    return JSONResponse({'result': str(prediction)})
+
+@app.route('/check', methods=['POST'])
+async def check(request):
+    data = await request.form()
+    img_bytes = await (data['file'].read())
+    img = open_image(BytesIO(img_bytes))
+    prediction_info = learn.predict(img)
+    prediction = prediction_info[0]
+    top_probs, top_classes = prediction_info[2].topk(1)
+    if top_probs.item() < 0.90:
+        return JSONResponse({'result': 'Seems like the image provided is not a cocoa tree'})
+    else:
+        return JSONResponse({'result': str(prediction)})
+
+@app.route('/try')
+async def tried(request):    
+    return JSONResponse({'result': 'ok'})
+
+@app.route('/about')
+def about_page(request):
+    html = path/'view'/'about.html'
+    return HTMLResponse(html.open().read())
+
+if __name__ == '__main__':
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5000, log_level="info")
